00a92839eaa2966319ac8ac422515b4c70bd300a
Dmitriy-68 <d73-68@yandex.ru>
2024-12-09 22:55:25 +0300

    Модуль 16 задание 5 Список пользователей в шаблоне

---
+++ module_16_5.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, Path, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel, Field
+from typing import List, Annotated
+from fastapi.responses import HTMLResponse
+
+
+
+class User(BaseModel):
+    id: int
+    username: str = Field(..., min_length=5, max_length=20, description='Username')
+    age: int = Field(..., ge=18, le=120, description='User age')
+
+
+app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True}, debug=True)
+templates = Jinja2Templates('templates')
+
+users = []
+
+
+@app.get('/', response_class=HTMLResponse)
+async def get_all_users(request: Request):
+    return templates.TemplateResponse("users.html", {"request": request, "users": users})
+
+
+@app.get('/user/{user_id}', response_class=HTMLResponse)
+async def get_user(request: Request, user_id: int):
+    for user in users:
+        if user.id == user_id:
+            return templates.TemplateResponse("users.html", {"request": request, "user": user})
+    return users
+
+
+@app.post('/user/{username}/{age}', response_model=User)
+async def register_user(username: str, age: int) -> User:
+    if len(users):
+        user_id = users[-1].id + 1
+    else:
+        user_id = 1
+    new_user = User(id=user_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
+
+
+@app.put('/user/{user_id}/{username}/{age}', response_model=User)
+async def update_user(user_id: int, username: str, age: int) -> User:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.delete('/user/{user_id}', response_model=User)
+async def delete_user(user_id: int) -> User:
+    for i, user in enumerate(users):
+        if user.id == user_id:
+            del users[i]
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
